9ee847561003d719e703c14d46cb768d9f9f8c00
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-06-11 17:20:05 +0530

    cors

--- mock/mockapi.js
+++ mock/mockapi.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const cors = require("cors");
+
+
+app.use(
+  cors({
+    origin: "https://app-crm-beta.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
